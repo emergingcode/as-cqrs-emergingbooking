0d6ff9a0a9c30a0d02d6a3ba38c256d89d7fd95d
José Roberto Araújo <jroberto.araujo@gmail.com>
2021-07-11 23:47:43 +0100

    Atualizadas configuracoes do Kafka e Sql Server no docker-compose e corrigida forma de instanciar a Lazy<IDocumentStore> do RavenDB

--- dev-setup/start-environment.ps1
+++ dev-setup/start-environment.ps1
@@ -14,7 +14,7 @@ if ($DebugMode) {
 
 docker-compose -f docker-compose.yaml up $DebugModeCommand;
 
-Write-Host "Waiting $WaitTimeForContainersUp before starting to create topics"
+Write-Host "Waiting $WaitTimeForContainersUp sec. before starting to create topics"
 
 Start-Sleep -s $WaitTimeForContainersUp
 
@@ -22,11 +22,10 @@ Write-Host "Creating Topics"
 
 foreach ($topic in $TopicNames) {
   Write-Host "Creating topic: $topic"
-  $CreateTopicCommand = "kafka-topics --create --topic " + $topic + " --partitions 3 --replication-factor 1 --if-not-exists --zookeeper  zookeeper:22181"
+  $CreateTopicCommand = "kafka-topics --create --topic " + $topic + " --partitions 3 --replication-factor 1 --if-not-exists --zookeeper  zoo1:2181"
   docker exec -it dev-setup_kafka_1 bash -c "$CreateTopicCommand"
-  Write-Host "Waiting 3s before creating another topic"
+  Write-Host "Waiting 3 sec. before creating another topic"
   Start-Sleep -s 3
-  #Write-Host $CreateTopicCommand
 }
 
 Write-Host "Topics were created"
